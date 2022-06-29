9d1a2f1b45daf9dc283b193d29cec87fc1cb305a
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-06-29 14:58:54 +0400

    Insert Data with Mongoose

---
+++ index.js
@@ -0,0 +1,38 @@
+require('./config.js');
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const dbname = 'conFusion';
+const url = `mongodb+srv://conFusion:${DB_PASS}@confusion.lnvgz.mongodb.net/?retryWrites=true&w=majority`
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
